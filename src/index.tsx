d726f4a769742e546ee74530f683e6aaf9a025c1
jwinton@veroskills.com <jrandallwinton@gmail.com>
2023-02-14 16:57:47 -0500

    start to clean up the primitives

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+'use client';
+
 import React from 'react';
 import { create } from 'zustand';
 
+const IS_SERVER = typeof window === 'undefined';
+const useIsomorphicLayoutEffect = IS_SERVER
+  ? React.useEffect
+  : React.useLayoutEffect;
+
+// Create a default state for the lightbox
 export interface LightboxState {
   isOpen: boolean;
   items: number[];
@@ -9,6 +17,7 @@ export interface LightboxState {
   setCurrentItem: (index: number) => void;
 }
 
+// create a Zustand store, declared as a React hook
 export const useLightboxStore = create<LightboxState>((set) => ({
   isOpen: false,
   items: [1, 2, 3, 4, 5],
@@ -17,27 +26,31 @@ export const useLightboxStore = create<LightboxState>((set) => ({
   setCurrentItem: (index: number) => set(() => ({ currentItem: index })),
 }));
 
-const TinyboxWrapper = ({
-  children,
-  as: Wrapper = 'div',
-}: {
+// Lightbox wrapper component
+interface WrapperProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  as?: React.ElementType;
-}) => {
-  return <Wrapper>{children}</Wrapper>;
+}
+
+const Wrapper = ({ children, ...rest }: WrapperProps) => {
+  return <div {...rest}>{children}</div>;
 };
 
-const TinyboxItem = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+// Lightbox item component
+interface ItemProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+const Item = ({ children, ...rest }: ItemProps) => {
+  return <div {...rest}>{children}</div>;
 };
 
-const TinyboxNav = ({
-  children,
-  direction,
-}: {
+// Lightbox navigation component
+interface NavProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   direction: 'next' | 'previous';
-}) => {
+}
+
+const Nav = ({ children, direction, ...rest }: NavProps) => {
   const { items, currentItem, setCurrentItem, toggleOpen } = useLightboxStore();
 
   const handleNav = () => {
@@ -57,15 +70,17 @@ const TinyboxNav = ({
   };
 
   return (
-    <div>
-      <button onClick={handleNav}>{children}</button>
-    </div>
+    <button onClick={handleNav} {...rest}>
+      {children}
+    </button>
   );
 };
 
+// The whole shebang
 export const Tinybox = ({ children }: { children: React.ReactNode }) => {
-  return <TinyboxWrapper>{children}</TinyboxWrapper>;
+  return <Wrapper>{children}</Wrapper>;
 };
 
-Tinybox.Item = TinyboxItem;
-Tinybox.Nav = TinyboxNav;
+// Export the sub-components as static properties
+Tinybox.Item = Item;
+Tinybox.Navigation = Nav;
