ec8414b1b3af451db2b8d3d459d3d59d6caeed70
jwinton@veroskills.com <jrandallwinton@gmail.com>
2023-02-16 17:19:46 -0500

    update website

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,7 @@
 const Index = () => {
   return (
     <>
-      <h1 className="text-center text-lg text-neutral-200">
-        <em className="font-serif">tinylight</em> docs coming soon
-      </h1>
+      <h1 className="text-center text-lg text-neutral-200">docs coming soon</h1>
     </>
   );
 };
