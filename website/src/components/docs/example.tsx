909de1d10c7d433e25fb9e7b417ddeaa68e7cb59
Jesse Winton <jesse@appwrite.io>
2024-12-10 00:19:37 -0500

    update docs

--- website/src/components/docs/example.tsx
+++ website/src/components/docs/example.tsx
@@ -2,47 +2,19 @@
 
 import { Lightbox } from '@tinylight-ui/lightbox'
 import Image from 'next/image'
+import { Button } from '../button'
 
 export const Example = () => {
   return (
     <div className="space-y-2" id="example">
-      <h3>Example</h3>
+      <h3 className="font-bold">Example</h3>
 
       <Lightbox.Root>
-        <div className="flex justify-between gap-4 overflow-x-scroll [scrollbar-width:none]">
-          <Lightbox.Trigger className="[flex:0_0_50%]">
-            <Image
-              src="https://placehold.co/1400x2200/png"
-              width={1400}
-              height={2200}
-              alt="Placeholder"
-            />
-          </Lightbox.Trigger>
-          <Lightbox.Trigger className="[flex:0_0_50%]">
-            <Image
-              src="https://placehold.co/800x400/png"
-              width={800}
-              height={400}
-              alt="Placeholder"
-            />
-          </Lightbox.Trigger>
-          <Lightbox.Trigger className="[flex:0_0_50%]">
-            <Image
-              src="https://placehold.co/960x650/png"
-              width={960}
-              height={650}
-              alt="Placeholder"
-            />
-          </Lightbox.Trigger>
-          <Lightbox.Trigger className="[flex:0_0_50%]">
-            <Image
-              src="https://placehold.co/1300x500/png"
-              width={1300}
-              height={500}
-              alt="Placeholder"
-            />
-          </Lightbox.Trigger>
-        </div>
+        <Lightbox.Trigger asChild>
+          <Button className="mt-4 w-1/2 bg-neutral-100! text-neutral-900">
+            Open Lightbox
+          </Button>
+        </Lightbox.Trigger>
 
         <Lightbox.Content title="Lightbox" description="Lightbox desc">
           <Lightbox.Close aria-label="Close" />
